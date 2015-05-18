fe0d4623fcf0bc1303fc452e8991ac0f5cd3bc5c
Wilber <Wilber@192.168.1.2>
2015-05-18 16:48:26 -0500

    creando clase autor

--- Library/src/logica/Autor.java
+++ Library/src/logica/Autor.java
@@ -1,5 +1,29 @@
 package logica;
 
 public class Autor {
+	private int id;
+	private String nombre;
+
+	public Autor(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 }
--- Library/src/logica/GestorLibro.java
+++ Library/src/logica/GestorLibro.java
@@ -1,5 +1,64 @@
 package logica;
 
+import java.awt.Image;
+
 public class GestorLibro {
+	private int id;
+	private String titulo;
+	private Autor autor;
+	private String descrripcion;
+	private double valor;
+	private final Image portada;
+
+	public GestorLibro(int id, String titulo, Autor autor, String descrripcion,
+			double valor, Image portada) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.descrripcion = descrripcion;
+		this.valor = valor;
+		this.portada = portada;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+	public String getDescrripcion() {
+		return descrripcion;
+	}
+
+	public void setDescrripcion(String descrripcion) {
+		this.descrripcion = descrripcion;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
 
 }
