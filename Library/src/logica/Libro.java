edf3ac5d421601dff7131f6ec760d8c4b97fe80a
Wilber <Wilber@192.168.1.2>
2015-05-18 18:14:35 -0500

    CAMBIOS GESTOR LIBRO, AUTOR, LIBRO

--- Library/src/logica/Libro.java
+++ Library/src/logica/Libro.java
@@ -1,5 +1,61 @@
 package logica;
 
+
 public class Libro {
+	private int id;
+	private String titulo;
+	private Autor autor;
+	private String descrripcion;
+	private double valor;
+
+	public Libro(int id, String titulo, Autor autor, String descrripcion,
+			double valor) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.descrripcion = descrripcion;
+		this.valor = valor;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+	public String getDescrripcion() {
+		return descrripcion;
+	}
+
+	public void setDescrripcion(String descrripcion) {
+		this.descrripcion = descrripcion;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
 
 }
--- Library/src/logica/Usuario.java
+++ Library/src/logica/Usuario.java
@@ -1,5 +1,42 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Usuario {
+	private int id;
+	private String nombre;
+	private double presupuesto;
+	private ArrayList<Libro> listaLibros;
+
+	public Usuario(int id, String nombre, double presupuesto) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.presupuesto = presupuesto;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPresupuesto() {
+		return presupuesto;
+	}
+
+	public void setPresupuesto(double presupuesto) {
+		this.presupuesto = presupuesto;
+	}
 
 }
