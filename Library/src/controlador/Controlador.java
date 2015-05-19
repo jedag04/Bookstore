fd385983b5d9c31212dbb46f34710b941e74a21c
Wilber <Wilber@ASUS>
2015-05-19 12:16:26 -0500

    .a

--- Library/src/controlador/Controlador.java
+++ Library/src/controlador/Controlador.java
@@ -10,28 +10,28 @@ import presentacion.DialogoAgregarUsuario;
 import presentacion.DialogoUsuario;
 import presentacion.Ventana;
 
-public class Controlador implements ActionListener{
-	
+public class Controlador implements ActionListener {
+
 	public static final String A_ABRIR_ADMINISTRADOR = "ABRIR_ADMINISTRADOR";
 	public static final String A_ABRIR_USUARIO = "ABRIR_USUARIO";
 	public static final String A_AGREGAR_LIBRO = "AGREGAR_LIBRO";
 	public static final String A_AGREGAR_USUARIO = "AGREGAR_USUARIO";
 	public static final String A_AGREGAR_AUTOR = "AGREGAR_AUTOR";
-	private Ventana ventana;
-	private DialogoAdministrador dialogoAdministrador;
-	private DialogoUsuario dialogoUsuario;
-	private DialogoAgregarLibro dialogoAgregarLibro;
-	private DialogoAgregarUsuario dialogoAgregarUsuario;
-	private DialogoAgregarAutor dialogoAgregarAutor;
+	private final Ventana ventana;
+	private final DialogoAdministrador dialogoAdministrador;
+	private final DialogoUsuario dialogoUsuario;
+	private final DialogoAgregarLibro dialogoAgregarLibro;
+	private final DialogoAgregarUsuario dialogoAgregarUsuario;
+	private final DialogoAgregarAutor dialogoAgregarAutor;
 
 	public Controlador() {
 		ventana = new Ventana(this);
-		dialogoAdministrador = new DialogoAdministrador(ventana,this);
+		dialogoAdministrador = new DialogoAdministrador(ventana, this);
 		dialogoUsuario = new DialogoUsuario(ventana, this);
 		dialogoAgregarLibro = new DialogoAgregarLibro(ventana, this);
 		dialogoAgregarUsuario = new DialogoAgregarUsuario(ventana, this);
 		dialogoAgregarAutor = new DialogoAgregarAutor(ventana, this);
-		
+
 		ventana.setVisible(true);
 	}
 
@@ -41,15 +41,15 @@ public class Controlador implements ActionListener{
 		case A_ABRIR_ADMINISTRADOR:
 			dialogoAdministrador.setVisible(true);
 			break;
-		
+
 		case A_ABRIR_USUARIO:
 			dialogoUsuario.setVisible(true);
 			break;
-			
+
 		case A_AGREGAR_LIBRO:
 			dialogoAgregarLibro.setVisible(true);
 			break;
-			
+
 		case A_AGREGAR_USUARIO:
 			dialogoAgregarUsuario.setVisible(true);
 			break;
@@ -61,10 +61,10 @@ public class Controlador implements ActionListener{
 			break;
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
 		Controlador c = new Controlador();
-		
+
 	}
 }
